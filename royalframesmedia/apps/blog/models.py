33a76e12167d137b5d3ef724fa1192780ae23f8a
jamesbeamie <wafulajames9@gmail.com>
2019-09-20 08:33:07 +0300

    Update changes

--- royalframesmedia/apps/blog/models.py
+++ royalframesmedia/apps/blog/models.py
@@ -16,6 +16,8 @@ class Blog(models.Model):
     image_path = models.CharField(max_length=255, blank=True, null=True)
     slug = models.SlugField(max_length=255)
     title = models.CharField(db_index=True, max_length=255)
+    tag = models.CharField(db_index=True, max_length=255)
+    description = models.CharField(db_index=True, max_length=255)
     body = models.CharField(db_index=True, max_length=8055)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
