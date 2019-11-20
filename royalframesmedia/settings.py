1b92673d79a2886dd7e9d36bded57f9d24ac497d
James <admin@James.local>
2019-11-20 15:36:07 +0300

    Cors

--- royalframesmedia/settings.py
+++ royalframesmedia/settings.py
@@ -145,6 +145,7 @@ STATIC_URL = '/static/'
 CORS_ORIGIN_WHITELIST = (
     '0.0.0.0:4000',
     'localhost:4000',
+    'https://royalframesmedia-api.herokuapp.com/photography/royalframesmedia/',
 )
 
 # Tell Django about the custom `User` model we created. The string
