9379d7132dfb9a835a6b3950591a5d5a12dd3e2f
James <admin@James.local>
2020-01-08 11:04:49 +0300

    DB to heroku postgres

--- royalframesmedia/settings.py
+++ royalframesmedia/settings.py
@@ -13,9 +13,15 @@ https://docs.djangoproject.com/en/2.1.4/ref/settings/
 import os
 import django_heroku
 import datetime
+import dj_database_url
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1.4/howto/deployment/checklist/
@@ -68,6 +74,7 @@ MIDDLEWARE = [
     'django.middleware.common.BrokenLinkEmailsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -99,12 +106,9 @@ WSGI_APPLICATION = 'royalframesmedia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1.4/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1.4/ref/settings/#auth-password-validators
@@ -141,6 +145,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+#static files storage
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ORIGIN_WHITELIST = [
     'https://royalframesmedia-api.herokuapp.com',
@@ -212,7 +218,8 @@ CLOUDINARY = {
 }
 
 # Activate Django-Heroku.
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
