23b5bf208576f547382733b006d7cd1782889f31
James <admin@James.local>
2020-01-08 21:01:18 +0300

    pagination fifety

--- royalframesmedia/settings.py
+++ royalframesmedia/settings.py
@@ -173,7 +173,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     #This should be fixed on the frontend at most 12
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 20
 }
 
 # Django-Rest Framework Jwt settings
