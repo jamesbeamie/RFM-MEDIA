c5585ec95f5efea22aa7e3feb6fcf24c9a65ba2e
James <admin@James.local>
2020-01-08 19:38:44 +0300

    remove pagination

--- royalframesmedia/settings.py
+++ royalframesmedia/settings.py
@@ -171,7 +171,7 @@ REST_FRAMEWORK = {
     ),
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 1000
 }
 
 # Django-Rest Framework Jwt settings
